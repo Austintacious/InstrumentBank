df78de57e79db9a0f31adca342a1f1e4b3f0082c
Austin Winslow <awinslow21@gmail.com>
2014-02-03 18:59:49 -0500

    add tests, modify user relationship to instrumentbank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  has_one :instrumentbank
+  has_many :instruments,
+    through: :instrumentbank
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates :first_name, format: { with: /\A[a-z]+\z/i }
+  validates :last_name, format: { with: /\A[a-z]+\z/i }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:invalid_emails) {['urser', 'user@com', 'users.com']}
+  let(:invalid_names) {["12abc34", "abc123def", 123]}
+  let(:blanks) {['', nil]}
+
+  it {should have_many :instruments}
+  it {should have_one :instrumentbank}
+
+  it {should have_valid(:first_name).when("Albus")}
+  it {should_not have_valid(:first_name).when(*invalid_names, *blanks)}
+
+  it {should have_valid(:last_name).when("Dumbledore")}
+  it {should_not have_valid(:last_name).when(*invalid_names, *blanks)}
+
+  it {should have_valid(:email).when("mysteriousguy@wizard.com")}
+  it {should_not have_valid(:email).when(*invalid_emails, *blanks)}
+
+  it 'has a matching password confirmation' do
+    user = User.new
+    user.password = "password"
+    user.password_confirmation = "another_password"
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 end
